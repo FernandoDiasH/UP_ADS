f02b10dc79539e0fffddc2472312d5ff30d2339f
FernandoDiasH <fernandohiroshi10@hotmail.com>
2022-05-17 21:07:20 -0300

    Mais sobre orientação a objeto

---
+++ 3_semestre/Desenvolvimento_Software/aula005/aula005/src/br/edu/up/model/Carro.java
@@ -0,0 +1,18 @@
+package br.edu.up.model;
+
+import java.util.List;
+
+public class Carro extends MeioDeTransporte{
+	
+	public Motor motor;
+	public Roda rodaDianteiraEsquerda;
+	public Roda rodaDianteiraDireita;
+	public Roda rodaTraseiraDireita;
+	public Roda rodaTraseiraEsquerda;
+	public Roda rodaStep;
+	
+	
+	public List<Portas> portas;
+	
+
+}
